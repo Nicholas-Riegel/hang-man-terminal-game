4592640736c4beb2133d50daa5ce374fd97bac7d
Nicholas <nicholaspriegel@gmail.com>
2024-03-30 20:16:01 -0400

    v1 complete

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # hang-man-terminal-game
+
+I asked ChatGPT how to find if array of objects contains object with a specific value for a property?
+It asnwered with the following:
+
+"const hasObjectWithPropertyValue = arrayOfObjects.some(obj => obj.name === targetValue);
+In this example, arrayOfObjects.some() iterates through each object in the array and returns true if at least one object satisfies the condition (obj.name === targetValue), otherwise it returns false."
+
+I only need to check if the condition is true, so I used it in the following way:
+
+if (wordToGetArray.some(x => x.used === false)){
+    // if there is some word that hasn't been used yet, play again
+    // otherwise end the game
+}
--- app.js
+++ app.js
@@ -1,40 +1,94 @@
 const prompt = require("prompt-sync")()
 
-const words = ["radar"]
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const wordsArray = [
+	{
+		word: 'elephant',
+		used: false
+	},
+	{
+		word: 'sunshine',
+		used: false
+	},
+	{
+		word: 'rainbow',
+		used: false
+	},
+	{
+		word: 'bicycle',
+		used: false
+	},
+	{
+		word: 'guitar',
+		used: false
+	},
+	{
+		word: 'chocolate',
+		used: false
+	},
+	{
+		word: 'adventure',
+		used: false
+	},
+	{
+		word: 'butterfly',
+		used: false
+	},
+	{
+		word: 'castle',
+		used: false
+	},
+	{
+		word: 'waterfall',
+		used: false
+	}
+]
+
+let wordToGet = ''
+let wordToGetArray = []
+let wrongAnswersArray = []
 
 player = {
-  name: ''
+	name: ''
 }
 
+// for(let i = 0; i < wordsArray.length - 1; i++){
+// 	wordsArray[i].used = true;
+// }
+
 const exitGame = () => {
+	console.clear()
 	process.exit();
 }
 
-const decision = (letter) => {
-
-	let answer = ''
-	
-	while (!alphabet.includes(answer)){
-		if(answer === 'exit') exitGame();
-		answer = prompt(`Please type a letter (or type 'exit' to exit the game): `);
-	}
-	
+const display = () => {
 	console.clear()
-	
-	if (answer === letter){
-		console.log('this letter is in the word')
-	} else if (answer === option2){
-		console.log('this letter is not in the word')
+	console.log(`Welcome ${player.name}!`);
+	console.log(`Word to get: ${wordToGetArray}`);
+	console.log(`Wrong answers: ${wrongAnswersArray}`);	
+}
+
+const findUnusedWord = () => {
+	if (wordsArray.some(x => x.used === false)){
+		for(let i = 0; i < wordsArray.length; i++){
+			if (wordsArray[i].used === false){
+				wordToGet = wordsArray[i]['word']
+				break;
+			}
+		}
+		for(let i = 0; i < wordToGet.length; i++){
+			wordToGetArray.push('_')
+		}
 	} else {
-		exitGame();
+		console.log('END OF GAME!');// this is not showing
+		exitGame()
 	}
 }
 
 const beginGame = () => {
-
+	
 	console.clear()
-  
+	
 	console.log("Welcome to this Hangman game. Let's begin.")
 	
 	const username = prompt("Please type your name (or type 'exit' at anytime to exit the game): ")
@@ -43,44 +97,82 @@ const beginGame = () => {
 	
 	player.name = username;
 	
-	console.clear()
+
+	findUnusedWord()
 	
-	console.log(player)
-
-	console.log(`Welcome, ${username}!`)
-	
-  listWord()
-}
-
-const listWord = () => {
-  const spaces = []
-  for(let i = 0; i < words[0].length; i++){
-    spaces.push('_')
-  }
-  console.log(...spaces)
-
-  chooseLetter()
+	chooseLetter()
 }
 
 const chooseLetter = () => {
   
-  let answer = ''
+	display()
+
+  	let answer = ''
 	
 	while (!alphabet.includes(answer)){
 		if(answer === 'exit') exitGame();
 		answer = prompt(`Please type a letter (or type 'exit' to exit the game): `);
 	}
 	
-  console.log('you chose: ', answer)
-	// console.clear()
+	if (wordToGet.includes(answer)){
+		correctAnswer(answer)
+	} else {
+		wrongAnswer(answer)
+	}
+}
+
+const correctAnswer = (answer) => {
+	for(let i = 0; i < wordToGet.length; i++){
+		if (answer === wordToGet[i]){
+			wordToGetArray.splice(i, 1, answer)
+		}
+	}
+	if (!wordToGetArray.includes('_')){
+		wordsArray.forEach(x => {
+			if (x.word === wordToGet){
+				x.used = true
+			}
+		})
+		youWin()
+	} else {
+		chooseLetter()
+	}
+}
+
+const wrongAnswer = (answer) => {
+	wrongAnswersArray.push(answer)
+	chooseLetter()
+}
+
+const youWin = (params) => {
 	
-	// if (answer === letter){
-	// 	console.log('this letter is in the word')
-	// } else if (answer === option2){
-	// 	console.log('this letter is not in the word')
-	// } else {
-	// 	exitGame();
-	// }
+	if (wordsArray.some(x => x.used === false)){	
+		display()
+		console.log(`Congratulations! You win!`);
+		console.log('Would you like to play again?');
+	} else {
+		console.clear()
+		console.log('Congratulations. You win!');
+		console.log('END OF GAME!');
+		exitGame()
+	}
+	
+	let answer = ''
+	
+	while (answer !== 'yes' && answer !== 'no'){
+		if(answer === 'exit') exitGame();
+		answer = prompt(`Please type 'yes' or 'no' (or type 'exit' to exit the game): `);
+	}
+	
+	if (answer === 'yes'){
+		wordToGetArray = []
+		wrongAnswersArray = []
+		findUnusedWord()
+		chooseLetter()
+	} else {
+		exitGame()
+	}
 }
 
 beginGame();
+
