c9e2a599f66e469bf58741fb32df02cd8133356f
Nicholas <nicholaspriegel@gmail.com>
2024-03-31 10:39:05 -0400

    v2 complete

--- app.js
+++ app.js
@@ -4,71 +4,72 @@ const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm
 const wordsArray = [
 	{
 		word: 'elephant',
-		used: false
 	},
 	{
 		word: 'sunshine',
-		used: false
 	},
 	{
 		word: 'rainbow',
-		used: false
 	},
 	{
 		word: 'bicycle',
-		used: false
 	},
 	{
 		word: 'guitar',
-		used: false
 	},
 	{
 		word: 'chocolate',
-		used: false
 	},
 	{
 		word: 'adventure',
-		used: false
 	},
 	{
 		word: 'butterfly',
-		used: false
 	},
 	{
 		word: 'castle',
-		used: false
 	},
 	{
 		word: 'waterfall',
-		used: false
 	}
 ]
 
 let wordToGet = ''
 let wordToGetArray = []
 let wrongAnswersArray = []
+const numWrongAnswers = 5
 
 player = {
-	name: ''
+	name: '',
+	wins: 0,
+	losses: 0
 }
 
+// to check how end of game works:
 // for(let i = 0; i < wordsArray.length - 1; i++){
 // 	wordsArray[i].used = true;
 // }
 
 const exitGame = () => {
-	console.clear()
 	process.exit();
 }
 
 const display = () => {
 	console.clear()
-	console.log(`Welcome ${player.name}!`);
+	let numPlayed = 0
+	for (let i = 0; i < wordsArray.length; i++){
+		if (wordsArray[i].used === true){
+			numPlayed++
+		}
+	}
+	console.log(`Player: ${player.name}`);
+	console.log(`Games played: ${numPlayed}/${wordsArray.length}`);
+	console.log(`Wins: ${player.wins}. Losses: ${player.losses}`);
 	console.log(`Word to get: ${wordToGetArray}`);
 	console.log(`Wrong answers: ${wrongAnswersArray}`);	
 }
 
-const findUnusedWord = () => {
+const setWordToGetArray = () => {
 	if (wordsArray.some(x => x.used === false)){
 		for(let i = 0; i < wordsArray.length; i++){
 			if (wordsArray[i].used === false){
@@ -80,14 +81,25 @@ const findUnusedWord = () => {
 			wordToGetArray.push('_')
 		}
 	} else {
-		console.log('END OF GAME!');// this is not showing
+		console.log('END OF GAME!');
 		exitGame()
 	}
 }
 
+const setWrongAnswersArray = (num) => {
+	for(let i = 0; i < num; i++){
+		wrongAnswersArray.push('_')
+	}
+}
+
+
 const beginGame = () => {
 	
 	console.clear()
+
+	wordsArray.forEach(x => {
+		x.used = false
+	})
 	
 	console.log("Welcome to this Hangman game. Let's begin.")
 	
@@ -97,8 +109,10 @@ const beginGame = () => {
 	
 	player.name = username;
 	
+	
+	setWordToGetArray()
 
-	findUnusedWord()
+	setWrongAnswersArray(numWrongAnswers)
 	
 	chooseLetter()
 }
@@ -140,19 +154,35 @@ const correctAnswer = (answer) => {
 }
 
 const wrongAnswer = (answer) => {
-	wrongAnswersArray.push(answer)
-	chooseLetter()
+	for (let i = 0; i < wrongAnswersArray.length; i++){
+		if (wrongAnswersArray[i] === '_'){
+			wrongAnswersArray.splice(i, 1, answer)
+			break;
+		}
+	}
+	if (!wrongAnswersArray.includes('_')){
+		wordsArray.forEach(x => {
+			if (x.word === wordToGet){
+				x.used = true
+			}
+		})
+		youLose()
+	} else {
+		chooseLetter()
+	}
 }
 
-const youWin = (params) => {
+const youWin = () => {
 	
+	player.wins++
+
 	if (wordsArray.some(x => x.used === false)){	
 		display()
 		console.log(`Congratulations! You win!`);
 		console.log('Would you like to play again?');
 	} else {
 		console.clear()
-		console.log('Congratulations. You win!');
+		display()
 		console.log('END OF GAME!');
 		exitGame()
 	}
@@ -167,7 +197,41 @@ const youWin = (params) => {
 	if (answer === 'yes'){
 		wordToGetArray = []
 		wrongAnswersArray = []
-		findUnusedWord()
+		setWordToGetArray()
+		setWrongAnswersArray(numWrongAnswers)
+		chooseLetter()
+	} else {
+		exitGame()
+	}
+}
+
+const youLose = () => {
+	
+	player.losses++
+	
+	if (wordsArray.some(x => x.used === false)){	
+		display()
+		console.log(`Sorry. You lost that game.`);
+		console.log('Would you like to play again?');
+	} else {
+		console.clear()
+		display()
+		console.log('END OF GAME!');
+		exitGame()
+	}
+	
+	let answer = ''
+	
+	while (answer !== 'yes' && answer !== 'no'){
+		if(answer === 'exit') exitGame();
+		answer = prompt(`Please type 'yes' or 'no' (or type 'exit' to exit the game): `);
+	}
+	
+	if (answer === 'yes'){
+		wordToGetArray = []
+		wrongAnswersArray = []
+		setWordToGetArray()
+		setWrongAnswersArray(numWrongAnswers)
 		chooseLetter()
 	} else {
 		exitGame()
